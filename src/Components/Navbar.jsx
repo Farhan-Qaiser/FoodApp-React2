9348b37fe142e72823def4e9d3ac9dc46dd75d14
Farhan-Qaiser <farhanqaiser1019@gmail.com>
2024-09-02 12:21:50 +0500

    Navbar component made and styles with tailwind css

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
         <nav className="flex justify-between px-8 sm:flex-row flex-col">
         <div className="flex items-center justify-center">
             <img src={Img} className="w-[60px]"/>
-            <h1 className="font-bold text-gray-600 text-lg ml-2">TASTY EATS</h1>
+            <h1 className="font-bold text-gray-600 text-lg ml-2">TASTY EATS CAFE</h1>
         </div>
         <div className="flex gap-x-5 justify-center items-center ">
             <h1 className="text-gray-600 font-semibold hover:text-orange-600 hover:cursor-pointer transition-all hover:pb-2">Home</h1>
